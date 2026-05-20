8d61c7155144efea730d7c7488ef60d02a05c223
sambbathiemassamba-lgtm <sambbathiemassamba@gmail.com>
2026-05-20 10:36:58 +0200

    connection a la nase de donnee

---
+++ pdo.php
@@ -0,0 +1,19 @@
+<?php 
+
+try{
+    $host = "mysql:host=localhost;dbname=waKAroma;charset=utf8";
+    $user = 'samzo';
+    $password = 'Touba:55';
+
+    $pdo = new PDO($host, $user, $password, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ
+    ]);
+
+}catch(Exception $e)
+{
+    die($e->getMessage());
+}
+
+
+?>
